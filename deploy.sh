cfcef8a3ea6e985d738decac82c3d31fcb84da8e
Sara Vieira <sara.vieira@mindera.com>
2017-08-07 22:46:43 +0100

    add trvis

--- deploy.sh
+++ deploy.sh
@@ -22,10 +22,7 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+git checkout $TARGET_BRANCH
 
 # Clean out existing contents
 rm -rf styleguide/**/* || exit 0
